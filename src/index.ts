0e30ae748d469fef56279aabc952311a5a82022e
nohehf <nohe.hinniger.foray@gmail.com>
2022-07-26 08:47:02 +0200

    feat/add-flags-support

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import { createPostmanCollection, saveJsonFormatted } from "./lib.ts";
+import { parse } from "https://deno.land/std@0.149.0/flags/mod.ts";
 
 function help() {
   console.log(`Error: not enough arguments.
 Usage:
-	deno run index.ts [graphql endpoint url] [output file name]?
+	deno run index.ts <GRAPHQL_ENDPOINT_URL> {--out=OUTPUT_FILE}
+Help:
+  deno run index.ts [--help | -h]
 `);
 }
 
-if (Deno.args.length < 1) {
+const args = parse(Deno.args, { boolean: ["help", "h"] }) as {
+  _: [string];
+  out?: string;
+  help?: boolean;
+  h?: boolean;
+};
+
+if (Deno.args.length < 1 || args.help || args.h) {
   help();
   Deno.exit(1);
 }
 
-const url = Deno.args[0];
-const filename = Deno.args[1];
+const url = args._[0];
+const filename = args.out;
 
 const urlRegexp = /https?:\/\/*/;
 if (!urlRegexp.test(url)) {
@@ -29,4 +39,3 @@ saveJsonFormatted(collection, outName);
 
 console.log(`Collection saved as ${outName}`);
 console.log(`Import it in postman and complete the queries ! 🚀`);
-
