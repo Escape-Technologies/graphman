a744a8e0e228716f1c671a90bfa010d3f888dd9d
nohehf <64989428+nohehf@users.noreply.github.com>
2022-07-26 08:18:02 +0200

    Revert "feat: add custom output file name support"

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { createPostmanCollection, saveJsonFormatted } from "./lib.ts";
 function help() {
   console.log(`Error: not enough arguments.
 Usage:
-	deno run index.ts [graphql endpoint url] [output file name]?
+	deno run index.ts [graphql endpoint url]
 `);
 }
 
@@ -13,8 +13,6 @@ if (Deno.args.length < 1) {
 }
 
 const url = Deno.args[0];
-const filename = Deno.args[1];
-
 const urlRegexp = /https?:\/\/*/;
 if (!urlRegexp.test(url)) {
   console.error(`${url} is not a valid url`);
@@ -24,9 +22,8 @@ console.log(`Creating the postman collection for ${url}`);
 
 const collection = await createPostmanCollection(url);
 
-const outName = filename || collection.info.name + ".postman_collection.json";
+const outName = collection.info.name + ".postman_collection.json";
 saveJsonFormatted(collection, outName);
 
 console.log(`Collection saved as ${outName}`);
 console.log(`Import it in postman and complete the queries ! 🚀`);
-
