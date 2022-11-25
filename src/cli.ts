369f016978c63ca7396a0276502b150078a3f50f
nohehf <nohe.hinniger.foray@gmail.com>
2022-11-25 10:40:40 +0100

    fix: change out arg check

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,9 @@
 import { parse } from "https://deno.land/std@0.149.0/flags/mod.ts";
 import { saveJsonFormatted } from "./lib.ts";
-import { ensureDirSync } from "https://deno.land/std@0.151.0/fs/mod.ts";
+import {
+  ensureDirSync,
+  ensureFileSync,
+} from "https://deno.land/std@0.151.0/fs/mod.ts";
 import { createPostmanCollection } from "./index.ts";
 
 // @TODO: improve the CLI
@@ -37,18 +40,18 @@ if (!urlRegexp.test(url)) {
   Deno.exit(1);
 }
 
-const pathRegexp = /^(.+)\/([^\/]+).json$/;
-if (path && !pathRegexp.test(path)) {
-  console.error(`${path} is not a valid path. It must be a json file path.`);
-  Deno.exit(1);
-}
-
 console.log(`Creating the postman collection for ${url}`);
 
 const collection = await createPostmanCollection(url, authorization);
 
 path = path || "./out/" + collection.info.name + ".postman_collection.json";
 path && ensureDirSync("./out/");
+try {
+  !path && ensureFileSync(path);
+} catch (e) {
+  console.error(`Error: ${e.message}`);
+  Deno.exit(1);
+}
 saveJsonFormatted(collection, path);
 console.log(`Collection saved at ${path}`);
 
